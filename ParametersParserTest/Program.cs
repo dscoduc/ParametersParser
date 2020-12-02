53fb88eae6bda69c2ad72dec205153d6b20f6417
Chris Blankenship <Chris.Blankenship@rackspace.com>
2020-12-02 15:54:45 -0800

    Initial Commit

---
+++ ParametersParserTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ParameterParserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parameters = new ParametersParser.Parameters();
+            foreach (var parameter in parameters)
+            {
+                Console.WriteLine("Index   : " + parameter.Index);
+                Console.WriteLine("Bruto   : " + parameter.Bruto);
+                Console.WriteLine("Netto   : [" + parameter.Netto + "]");
+                Console.WriteLine("Key     : " + parameter.Key);
+                Console.WriteLine("Value   : [" + (parameter.Value == null ? "<null>" : parameter.Value) + "]");
+                Console.WriteLine("HasValue: " + parameter.HasValue);
+                Console.WriteLine("");
+            }
+
+            string testValue = parameters.GetFirstValue("-test");
+
+            var t = parameters.FirstOrDefault(x => x.Key == "-Test");
+        }
+    }
+}
